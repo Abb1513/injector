117cc232016111b99f3465f9ba7ec66fddec7b28
abb1513 <wangchaoqun@fuse.co.id>
2021-04-16 16:09:33 +0800

    去除 log

--- BeanMapper.go
+++ BeanMapper.go
@@ -8,8 +8,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"reflect"
 )
 
@@ -17,14 +15,10 @@ type BeanMapper map[reflect.Type]reflect.Value
 
 func (this BeanMapper) add(bean interface{}) {
 	t := reflect.TypeOf(bean)
-	log.Println(t.Name())
-	log.Println(t.Kind())
 	if t.Kind() != reflect.Ptr {
 		panic("Must Prt")
 	}
 	this[t] = reflect.ValueOf(bean)
-	log.Println(this)
-	this.show()
 }
 
 func (this BeanMapper) get(bean interface{}) reflect.Value {
@@ -35,21 +29,14 @@ func (this BeanMapper) get(bean interface{}) reflect.Value {
 	} else {
 		t = reflect.TypeOf(bean)
 	}
-	log.Println(t.Name())
 	if v, ok := this[t]; ok {
 		return v
 	}
 	// 接口集成
-	for k,v := range this {
-		if t.Kind() == reflect.Interface && k.Implements(t)  {
+	for k, v := range this {
+		if t.Kind() == reflect.Interface && k.Implements(t) {
 			return v
 		}
 	}
 	return reflect.Value{}
-}
-
-func (this BeanMapper)show () {
-	for k,v := range this {
-		fmt.Println(k.Name(),v.Kind())
-	}
 }
